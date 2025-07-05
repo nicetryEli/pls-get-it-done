2c1e7b146dc59c0979b60292017ffb5ee9443baa
nicetryEli <annalilly131205@gmail.com>
2025-07-05 23:02:09 +0700

    bombadio crocodio

---
+++ plsgetitdone/src/main/java/com/lillysplan/plsgetitdone/PlsgetitdoneApplication.java
@@ -0,0 +1,13 @@
+package com.lillysplan.plsgetitdone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlsgetitdoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlsgetitdoneApplication.class, args);
+	}
+
+}
---
+++ plsgetitdone/src/test/java/com/lillysplan/plsgetitdone/PlsgetitdoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lillysplan.plsgetitdone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlsgetitdoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
